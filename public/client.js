cb127b493c50a4412e0a46d6f37eda657d9fe73c
Glitch (deadpan-crystal) <none>
2018-05-09 08:49:34 +0000

    🍡🌧 Checkpoint ./public/client.js:1073144/489

--- public/client.js
+++ public/client.js
@@ -21,23 +21,35 @@ var todoList = {
     var completedTodos = 0;
     
     // Get number of completed todos.
-    for (var i = 0; i < totalTodos; i++) {
-      if (this.todos[i].completed === true) {
+    // for (var i = 0; i < totalTodos; i++) {
+    //   if (this.todos[i].completed === true) {
+    //     completedTodos++;
+    //   }
+    // }
+    this.todo.forEach(function(todo){
+      if (todo.completed === true)
         completedTodos++;
-      }
-    }
+    });
     
     // Case 1: If everything’s true, make everything false.
-    if (completedTodos === totalTodos) {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = false;
-      }
-    // Case 2: Otherwise, make everything true.
+    // if (completedTodos === totalTodos) {
+    //   for (var i = 0; i < totalTodos; i++) {
+    //     this.todos[i].completed = false;
+    //   }
+    // // Case 2: Otherwise, make everything true.
+    // } else {
+    //   for (var i = 0; i < totalTodos; i++) {
+    //     this.todos[i].completed = true;
+    //   }      
+    // }
+    this.todos.forEach (function(todo){
+    if(completedTodos === true) {
+      todo.completed = false;
     } else {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = true;
-      }      
+      todo.completed = true;
     }
+      
+    });
   }
 };
 
